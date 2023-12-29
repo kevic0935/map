55fc9b3c10a815c0be31e57652491f5ee3a9c675
vic <vichsieh@sysage.com.tw>
2023-12-29 16:35:27 +0800

    marker type 定義

---
+++ src/app/map.ts
@@ -0,0 +1,20 @@
+// marker type 定義
+export interface marker {
+  label?: string;
+  url: string;
+  draggable: boolean;
+
+  updated_at: string; //最後更新時間
+  name_tw: string; //站別 ex.捷運科技大樓站
+  address_tw: string; //地址 ex.復興南路二段235號前
+  lat: number;
+  lng: number;
+  district_tw?: string; //區域 ex.大安區
+  district_en?: string; //區域 ex.Daan Dist
+  available_spaces_detail: { //此區 剩餘車
+    yb1: number; //1.0
+    yb2: number; //2.0
+  };
+  empty_spaces: number; //此區 空車位
+  parking_spaces: number; //此區 最多車位
+}
