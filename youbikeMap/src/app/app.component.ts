ae8f27b6573ed677a89f446cd1c12dfff1c9809f
vic <vichsieh@sysage.com.tw>
2024-01-02 18:16:07 +0800

    整理不必要的coding

--- youbikeMap/src/app/app.component.ts
+++ youbikeMap/src/app/app.component.ts
@@ -70,36 +70,4 @@ export class AppComponent {
     });
   }
 
-  // constructor(private map: MapsService) {
-  //   this.map.getData().subscribe((data: any) => {
-  //     let retVal: any[] = data.retVal;
-  //     retVal.forEach((x) => {
-  //       let marker: marker = {
-  //         // label: x.name_tw.replace(/YouBike2.0_/gi, ''), 刪除YouBike2.0 字眼
-  //         label: x.name_tw,
-  //         lat: x.lat,
-  //         lng: x.lng,
-  //         district_tw: x.district_tw,
-  //         available_spaces_detail: x.available_spaces_detail,
-  //         empty_spaces: x.empty_spaces,
-  //         url: 'https://maps.google.com/maps?ll=' +
-  //           x.lat +
-  //           ',' +
-  //           x.lng +
-  //           '&amp;z=17&amp;t=m&amp;hl=zh-TW&amp;gl=TW&amp;mapclient=embed&amp;cid=5172986590304905081',
-  //         draggable: true,
-  //         updated_at: '',
-  //         name_tw: '',
-  //         address_tw: '',
-  //         parking_spaces: 0
-  //       };
-  //       if (this.markers.length <= 20) {
-  //         this.markers.push(marker);
-  //       }
-  //     });
-  //     console.warn(this.markers);
-  //   });
-  // }
-
-
 }
